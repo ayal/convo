7629c7269f24c7b5e8fadd6692b66fcf9ef1447f
ayal <ayalgelles@gmail.com>
2020-02-13 10:59:37 +0200

    Updates

---
+++ precache-manifest.af3da5032fa0a3d3d96e98410f5c5f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252762c221d446ba48bafaefd1b8d607",
+    "url": "/convo/index.html"
+  },
+  {
+    "revision": "5745e428adf42ec41da2",
+    "url": "/convo/static/css/main.8cccc650.chunk.css"
+  },
+  {
+    "revision": "90df31352093a5d6b362",
+    "url": "/convo/static/js/2.86122f26.chunk.js"
+  },
+  {
+    "revision": "6d24087b6e0981336b798c48680cdc7f",
+    "url": "/convo/static/js/2.86122f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5745e428adf42ec41da2",
+    "url": "/convo/static/js/main.9c36d4df.chunk.js"
+  },
+  {
+    "revision": "72a20105c5a899d16631",
+    "url": "/convo/static/js/runtime-main.5371c240.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/convo/static/media/logo.5d5d9eef.svg"
+  }
+]);
